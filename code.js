ea1cc00e5f6d4988f3540444ed3d35558675ddc8
John <john@jbrereton.com>
2021-05-01 10:12:29 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,3 +2,7 @@
  * @author John Brereton
  * @since 5/1/2021
  */
+
+// Create lists to represent datasets
+var countryName = getColumn("Country Data", "Country name");
+var countryCode = getColumn("Country Data", "Country code");
