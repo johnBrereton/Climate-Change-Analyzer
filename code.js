61e1dedab55a3777aeeaabc26023e7a85bfae4cc
John <john@jbrereton.com>
2021-05-06 06:58:20 -0400

    Update code.js

--- code.js
+++ code.js
@@ -5,23 +5,23 @@
  */
 
 // Create lists to represent datasets
-var countryCode = getColumn("Country Data", "Country code");
-var countryName = getColumn("Country Data", "Country name");
-var seriesCode = getColumn("Country Data", "Series code");
-var seriesName = getColumn("Country Data", "Series name");
-var countryName1 = getColumn("Countries and Territories", "Country Name")
-var countryCode1 = getColumn("Countries and Territories", "Three Letter Country Code");
-var twoLetterCode = getColumn("Countries and Territories", "Two Letter Country Code");
-var region = getColumn("Countries and Territories", "Region");
-var incomeLevel = getColumn("Countries and Territories", "Income Level");
-var population = getColumn("Countries and Territories", "Population");
-var fertilityRate = getColumn("Countries and Territories", "Population");
-var unemployment = getColumn("Countries and Territories", "Unemployment");
-var gdp = getColumn("Countries and Territories", "GDP Per Capita");
-var percentUsingInternet = getColumn("Countries and Territories", "Percent Using Internet");
-var renewableEnergy = getColumn("Countries and Territories", "Percent Renewable Energy");
-var co2 = getColumn("Countries and Territories", "CO2 Emissions");
-var flag = getColumn("Countries and Territories", "Flag")
+var countryCodeList = getColumn("Country Data", "Country code");
+var countryNameList = getColumn("Country Data", "Country name");
+var seriesCodeList = getColumn("Country Data", "Series code");
+var seriesNameList = getColumn("Country Data", "Series name");
+var countryNameList1 = getColumn("Countries and Territories", "Country Name")
+var countryCodeList1 = getColumn("Countries and Territories", "Three Letter Country Code");
+var twoLetterCodeList = getColumn("Countries and Territories", "Two Letter Country Code");
+var regionList = getColumn("Countries and Territories", "Region");
+var incomeLevelList = getColumn("Countries and Territories", "Income Level");
+var populationList = getColumn("Countries and Territories", "Population");
+var fertilityRateList = getColumn("Countries and Territories", "Population");
+var unemploymentList = getColumn("Countries and Territories", "Unemployment");
+var gdpList = getColumn("Countries and Territories", "GDP Per Capita");
+var percentUsingInternetList = getColumn("Countries and Territories", "Percent Using Internet");
+var renewableEnergyList = getColumn("Countries and Territories", "Percent Renewable Energy");
+var co2List = getColumn("Countries and Territories", "CO2 Emissions");
+var flagList = getColumn("Countries and Territories", "Flag")
 
 // Create a list of yearly info from the dataset
 /* var yearlyInfo = [];
@@ -29,48 +29,59 @@ for (var i=1990; i < 2012; i++) {
     yearlyInfo[i] = getColumn("Country Data", i.toString());
 } */
 
-/**
- _______   ___      ___ _______   ________   _________        ___  ___  ________  ________   ________  ___       _______   ________  ________      
-|\  ___ \ |\  \    /  /|\  ___ \ |\   ___  \|\___   ___\     |\  \|\  \|\   __  \|\   ___  \|\   ___ \|\  \     |\  ___ \ |\   __  \|\   ____\     
-\ \   __/|\ \  \  /  / | \   __/|\ \  \\ \  \|___ \  \_|     \ \  \\\  \ \  \|\  \ \  \\ \  \ \  \_|\ \ \  \    \ \   __/|\ \  \|\  \ \  \___|_    
- \ \  \_|/_\ \  \/  / / \ \  \_|/_\ \  \\ \  \   \ \  \       \ \   __  \ \   __  \ \  \\ \  \ \  \ \\ \ \  \    \ \  \_|/_\ \   _  _\ \_____  \   
-  \ \  \_|\ \ \    / /   \ \  \_|\ \ \  \\ \  \   \ \  \       \ \  \ \  \ \  \ \  \ \  \\ \  \ \  \_\\ \ \  \____\ \  \_|\ \ \  \\  \\|____|\  \  
-   \ \_______\ \__/ /     \ \_______\ \__\\ \__\   \ \__\       \ \__\ \__\ \__\ \__\ \__\\ \__\ \_______\ \_______\ \_______\ \__\\ _\ ____\_\  \ 
-    \|_______|\|__|/       \|_______|\|__| \|__|    \|__|        \|__|\|__|\|__|\|__|\|__| \|__|\|_______|\|_______|\|_______|\|__|\|__|\_________\
-                                                                                                                                       \|_________|
-*/
+var resultsPage = 0;
+var userInput;
 
 onEvent("searchButton", "click", function() {
     generateResults();
 });
 
 onEvent("homeScreen", "keydown", function(event) {
-    generateResults();
+    if(event.key == "Enter") {
+        generateResults();
+    }
 });
 
-/**
- ________ ___  ___  ________   ________ _________  ___  ________  ________   ________      
-|\  _____\\  \|\  \|\   ___  \|\   ____\\___   ___\\  \|\   __  \|\   ___  \|\   ____\     
-\ \  \__/\ \  \\\  \ \  \\ \  \ \  \___\|___ \  \_\ \  \ \  \|\  \ \  \\ \  \ \  \___|_    
- \ \   __\\ \  \\\  \ \  \\ \  \ \  \       \ \  \ \ \  \ \  \\\  \ \  \\ \  \ \_____  \   
-  \ \  \_| \ \  \\\  \ \  \\ \  \ \  \____   \ \  \ \ \  \ \  \\\  \ \  \\ \  \|____|\  \  
-   \ \__\   \ \_______\ \__\\ \__\ \_______\  \ \__\ \ \__\ \_______\ \__\\ \__\____\_\  \ 
-    \|__|    \|_______|\|__| \|__|\|_______|   \|__|  \|__|\|_______|\|__| \|__|\_________\
-                                                                               \|_________|
-*/
+// Returns to the home screen when the results screen back button is pressed
+onEvent("resultsBackButton", "click", function() {
+    setScreen("homeScreen");
+    resultsPage = 0;
+});
+
+// Returns to the results screen when the info screen back button is pressed
+onEvent("infoBackButton", "click", function() {
+    setScreen("homeScreen");
+});
+
+// Changes to the previous page when the previous arrow is clicked
+onEvent("prevButton", "click", function() {
+    if(resultsPage>0) {
+        resultsPage--;
+        updateResults(userInput);
+    }
+});
+
+// Changes to the next page when the next arrow is clicked
+onEvent("nextButton", "click", function() {
+    if(resultsPage<updateResults(userInput)-1) {
+        resultsPage++;
+        updateResults(userInput);
+    }
+});
 
 // Validates that the country the user enters is valid
 // Provides the uesr with an error if the input is not valid
 function validateSearch() {
-    if(checkFor(getText("countryInput"), countryName1)) {
+    userInput = getText("countryInput");
+    if(checkFor(userInput, countryNameList1)) {
         setProperty("inputWarning", "text", "");
         return "country";
     }
-    else if(checkFor(getText("countryInput"), region)) {
+    else if(checkFor(userInput, regionList)) {
         setProperty("inputWarning", "text", "");
         return "region";
     }
-    else if(getText("countryInput") == "") {
+    else if(userInput == "") {
         setProperty("inputWarning", "text", "This is a required field");
         return false;
     }
@@ -82,12 +93,80 @@ function validateSearch() {
 
 // Updates the info screen with the info of the country selected by the user
 function updateInfo(country) {
-    var countryId = find(country, countryName);
-    var countryId1 = find(country, countryName1);
-    setProperty("countryOutput", "text", countryName1[countryId1]);
-    setProperty("flagImage", "image", flag[countryId1]);
-    setProperty("populationOutput", "text", population[countryId1]);
-    setProperty("regionOutput", "text", region[countryId1]);
+    var countryId = find(country, countryNameList);
+    var countryId1 = find(country, countryNameList1);
+    setProperty("countryOutput", "text", countryNameList1[countryId1]);
+    setProperty("flagImage", "image", flagList[countryId1]);
+    setProperty("populationOutput", "text", populationList[countryId1]);
+    setProperty("regionOutput", "text", regionList[countryId1]);
+}
+
+function updateResults(region) {
+    var filteredCountryList = findCountriesIn(region)[0];
+    var filteredPopulationList = findCountriesIn(region)[1];
+    var filteredFlagList = findCountriesIn(region)[2];
+    var sortedPopulationIdList = sortList(filteredPopulationList)[1];
+    var pages = Math.ceil(filteredCountryList.length/5);
+    for (var i=0; i<=4; i++) {
+        if(filteredCountryList[sortedPopulationIdList[(5*resultsPage)+i]] != undefined) {
+            setProperty("result" + i, "hidden", false);
+            setProperty("flag" + i, "hidden", false);
+            setProperty("result" + i, "text", filteredCountryList[sortedPopulationIdList[(5*resultsPage)+i]]);
+            setProperty("flag" + i, "image", filteredFlagList[sortedPopulationIdList[(5*resultsPage)+i]]);
+        }
+        else {
+            setProperty("result" + i, "hidden", true);
+            setProperty("flag" + i, "hidden", true);
+        }
+    }
+    setProperty("pageLabel", "text", "Page: " + (resultsPage+1) + " of " + pages);
+    return pages;
+}
+
+function findCountriesIn(region) {
+    var sortedCountryList = [];
+    var sortedPopulationList = [];
+    var sortedFlagList = [];
+    for(var i=0; i < countryNameList1.length; i++) {
+        if(regionList[i] == region) {
+            appendItem(sortedCountryList, countryNameList1[i]);
+            appendItem(sortedPopulationList, populationList[i]);
+            appendItem(sortedFlagList, flagList[i]);
+        }
+    }
+    return [sortedCountryList, sortedPopulationList, sortedFlagList];
+}
+
+function sortList(list) {
+    var sortedList = [];
+    var sortedIds = [];
+    var temp = 0;
+    sortedList = list;
+    sortedIds = createNumericalList(0, list.length);
+    for (var i = 0; i < sortedList.length; i++) {
+        for (var j=i; j < sortedList.length; j++) {
+            if (sortedList[j] > sortedList[i]) {
+                temp = sortedIds[j];
+                sortedIds[j] = sortedIds[i];
+                sortedIds[i] = temp;
+
+                temp = sortedList[j];
+                sortedList [j] = sortedList[i];
+                sortedList[i] = temp;
+            }
+        }
+    }
+    return [sortedList, sortedIds];
+}
+
+// Creates a list of consecutive integers
+// Requires a starting(from) and ending(to) number
+function createNumericalList(from, to) {
+    var list = [];
+    for (var i=from; i<=to; i++) {
+        appendItem(list, i);
+    }
+    return list;
 }
 
 // Checks a list for an element and returns whether or not the item is in the list
@@ -119,10 +198,10 @@ function find(element, list) {
 function generateResults() {
     if(validateSearch() == "country") {
         setScreen("infoScreen");
-        updateInfo(getText("countryInput"));
+        updateInfo(userInput);
     }
     else if(validateSearch() == "region") {
         setScreen("resultsScreen");
-        updateResults(getText("countryInput"));
+        updateResults(userInput);
     }
 }
